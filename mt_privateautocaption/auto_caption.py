86a5bac0cef653f30fc10fb76e4cbeeee0099e6c
Percy <percy@railway.app>
2021-12-03 08:48:35 +0000

    Initial Commit

---
+++ mt_privateautocaption/auto_caption.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# (c) Shrimadhav U K & PR0FESS0R-99
+
+import os
+from pyrogram import Client, filters 
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
+from pyrogram.errors import FloodWait
+
+CAPTION_TEXT = """HI"""
+
+@Client.on_message(filters.media & filters.channel)
+async def caption(client, message: Message):
+    kopp, _ = get_file_id(message)
+    await message.edit(f"<b>{kopp.file_name}</b>\n\n{CAPTION_TEXT}")
+
+def get_file_id(msg: Message):
+    if msg.media:
+        for message_type in (
+            "photo",
+            "animation",
+            "audio",
+            "document",
+            "video",
+            "video_note",
+            "voice",
+            # "contact",
+            # "dice",
+            # "poll",
+            # "location",
+            # "venue",
+            "sticker"
+        ):
+            obj = getattr(msg, message_type)
+            if obj:
+                return obj, obj.file_id
